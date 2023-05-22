b04cf93da48e47137c7f6e26f7e978040320708e
steadily-worked <iamsangminpark@gmail.com>
2023-05-22 22:00:08 +0900

    initial commit with sample post

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.jsx",
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true,
+})
+
+module.exports = withNextra({
+  reactStrictMode: true,
+  cleanDistDir: true,
+})
