610df3e479946e3f60756c0982d3ed92009fd3bd
steadily-worked <iamsangminpark@gmail.com>
2023-06-08 11:29:03 +0900

    delete RSS

--- theme.config.jsx
+++ theme.config.jsx
@@ -19,7 +19,6 @@ export default {
   footer: (
     <small style={{ display: "block", marginTop: "8rem" }}>
       {new Date().getFullYear()} @ Sangmin Park.
-      <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
           float: right;
