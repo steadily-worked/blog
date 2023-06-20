a1121be11c846a29fee2457d7618287f3eff1a23
steadily-worked <iamsangminpark@gmail.com>
2023-06-20 18:54:24 +0900

    fix link issue

--- components/image-with-caption.tsx
+++ components/image-with-caption.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import Link from "next/link"
 import React from "react"
 import Image from "next/image"
 
@@ -7,7 +6,6 @@ type ImageWithCaptionProps = {
   title: string
   src: string
   alt: string
-  link?: string
   width: number
   height: number
 }
@@ -16,38 +14,31 @@ const ImageWithCaption = ({
   title,
   src,
   alt,
-  link,
   width,
   height,
 }: ImageWithCaptionProps) => {
   if (title !== undefined) {
     return (
-      <StyledLink href={link ?? '#'} target="_blank">
-        <ImageWrapper>
-          <Image
-            src={src}
-            alt={alt}
-            style={{ margin: "0" }}
-            width={width}
-            height={height}
-          />
-          <figcaption>{title}</figcaption>
-        </ImageWrapper>
-      </StyledLink>
+      <ImageWrapper>
+        <Image
+          src={src}
+          alt={alt}
+          style={{ margin: "0" }}
+          width={width}
+          height={height}
+        />
+        <figcaption>{title}</figcaption>
+      </ImageWrapper>
     )
   } else {
     return <Image src={src} alt={alt} />
   }
 }
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`
-
 const ImageWrapper = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
+  flex-direction: column;
   cursor: default;
 `
 
